61c540a805168ce54bebbcc913060a2ecd4ff6b1
chillibilli <y.dubrov@gmail.com>
2018-01-13 13:28:19 +0300

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+﻿var options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+var today  = new Date();
+
+var tomorrow = new Date();
+tomorrow.setDate(tomorrow.getDate() + 89);
+document.getElementById("the_date").innerHTML = tomorrow.toLocaleDateString("ru-RU",options);
