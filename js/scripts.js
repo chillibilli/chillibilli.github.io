4c05068a5e485d1914dcadfb5b85fa692cb120a2
chillibilli <y.dubrov@gmail.com>
2018-02-27 15:29:42 +0300

    Update scripts.js
    update date and time in parenthesis

--- js/scripts.js
+++ js/scripts.js
@@ -6,4 +6,4 @@ tomorrow.setDate(tomorrow.getDate() + 89);
 document.getElementById("the_date").innerHTML = tomorrow.toLocaleDateString("ru-RU",options);
 
 var options1 = {year: '2-digit', month: '2-digit', day: '2-digit', hour: '2-digit', minute: '2-digit' };
-document.getElementById("updated").innerHTML = today.toLocaleDateString("ru-RU",options1);
+document.getElementById("updated").innerHTML = '(' + today.toLocaleDateString("ru-RU",options1) + ')';
